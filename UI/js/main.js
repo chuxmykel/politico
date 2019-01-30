c1f865ab0f0383576e32e96bbb2c95068774b25a
Chukwudi <ngwobiachukwudi@gmail.com>
2019-01-30 12:28:52 +0100

    Bug(links): Fix all links
    - Changed branch name to match the task
    - Made links on admin page functional
    
    [Delivers #163522847]

--- UI/js/main.js
+++ UI/js/main.js
@@ -3,28 +3,57 @@ let loadFile = (event)=> {
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
 
-// Get the modal
 var modal = document.getElementById('myModal');
+var modal1 = document.getElementById('myModal1');
+var modal2 = document.getElementById('myModal2');
 
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
-// When the user clicks on the button, open the modal 
+
+
+
+
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }else if (event.target == modal1) {
+    modal1.style.display = "none";
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
   }
 }
+
+
+
+
