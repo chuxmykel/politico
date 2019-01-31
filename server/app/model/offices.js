6dbc370c13c18ec7a2e2a392c05d888ba7daf311
Chukwudi <ngwobiachukwudi@gmail.com>
2019-01-31 13:59:46 +0100

    chore(dataStructures): Set up data structures
    - set up data structures and fill with dummy data.
    
    [Delivers #163610038]

---
+++ server/app/model/offices.js
@@ -0,0 +1,20 @@
+/* eslint-disable comma-dangle */
+const parties = [
+  {
+    id: 1,
+    type: 'federal',
+    name: 'presidential'
+  },
+  {
+    id: 2,
+    type: 'legislative',
+    name: 'senatorial'
+  },
+  {
+    id: 3,
+    type: 'state',
+    name: 'gubernatorial'
+  },
+];
+
+export default parties;
