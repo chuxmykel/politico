af8abc9d86973fe42903d8d142b1758c361486aa
Chukwudi <ngwobiachukwudi@gmail.com>
2019-02-01 04:50:30 +0100

    chore(travis): Setup travis for continuous integration
    - enter required information in .travis.yml file
    [Delivers #163643168]

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,14 +6,14 @@ const router = express.Router();
 const partyEndPoint = '/api/v1/parties/';
 const officeEndPoint = '/api/v1/offices/';
 
-// Party
+// Party routes
 router.post(partyEndPoint, PartyController.addParty);
 router.get(partyEndPoint, PartyController.getAllParties);
 router.get(`${partyEndPoint}:id`, PartyController.getOneParty);
 router.put(`${partyEndPoint}:id`, PartyController.editParty);
 router.delete(`${partyEndPoint}:id`, PartyController.deleteParty);
 
-// Office
+// Office routes
 router.post(officeEndPoint, OfficeController.addOffice);
 router.get(officeEndPoint, OfficeController.getAllOffices);
 router.get(`${officeEndPoint}:id`, OfficeController.getOneOffice);
