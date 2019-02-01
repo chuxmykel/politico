2c9ce3de25d6f653769af36b5e65a83782632f0e
Chukwudi <ngwobiachukwudi@gmail.com>
2019-02-01 03:56:38 +0100

    feat(endpoints): implement get a specific political office
    - Handles GET/ requests to 'api/v1/offices/:id' and gets the matching political office
    [Delivers #163610486]

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,33 +6,16 @@ const router = express.Router();
 const partyEndPoint = '/api/v1/parties/';
 const officeEndPoint = '/api/v1/offices/';
 
-/**
-    Party routes
-*/
-
-// Create a party
+// Party
 router.post(partyEndPoint, PartyController.addParty);
-
-// Get all parties
 router.get(partyEndPoint, PartyController.getAllParties);
-
-// Get a specific party
 router.get(`${partyEndPoint}:id`, PartyController.getOneParty);
-
-// Edit a specific party
 router.put(`${partyEndPoint}:id`, PartyController.editParty);
-
-// Delete a specific party
 router.delete(`${partyEndPoint}:id`, PartyController.deleteParty);
 
-/**
-    Office routes
-*/
-
-// Create an office
+// Office
 router.post(officeEndPoint, OfficeController.addOffice);
-
-// Get all offices
 router.get(officeEndPoint, OfficeController.getAllOffices);
+router.get(`${officeEndPoint}:id`, OfficeController.getOneOffice);
 
 export default router;
